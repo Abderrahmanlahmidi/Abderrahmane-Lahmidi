f94ea27f2dd4c79fd10f2f6ad99b1ed9bfeb9abe
lahmidi <dvabderrahman@gmail.com>
2025-07-30 18:27:03 +0200

    add navbar path

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../../src/components/Navbar"
+import Navbar from "../components/Navbar"
 import Hero from "../sections/Hero"
 import About from "../sections/About"
 import Footer from "../components/Footer"
