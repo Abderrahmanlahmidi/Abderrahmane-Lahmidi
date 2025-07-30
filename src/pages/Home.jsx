6c58e34f796028f45f302e4e19a4a39d04c75c62
lahmidi <dvabderrahman@gmail.com>
2025-07-30 18:31:07 +0200

    add navbar paths

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import Navbar from "../components/Navbar"
-import Hero from "../sections/Hero"
-import About from "../sections/About"
-import Footer from "../components/Footer"
-import Skills from "../sections/skills"
-import Experiences from "../sections/Experiences"
-import Certificates from "../sections/Certificates"
-import Projects from "../sections/Projects"
+import Navbar from "../components/Navbar";
+import Hero from "../sections/Hero";
+import About from "../sections/About";
+import Footer from "../components/Footer";
+import Skills from "../sections/skills";
+import Experiences from "../sections/Experiences";
+import Certificates from "../sections/Certificates";
+import Projects from "../sections/Projects";
 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-900 text-white">
-      <Navbar />
+      <Navbar/>
       <Hero/>
       <About/>
       <Skills/>
